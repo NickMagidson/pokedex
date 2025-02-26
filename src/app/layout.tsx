c2b154743c1ab23bb7f36c2c73a3c633badfc1c0
Nick <nickmagi24@gmail.com>
2025-02-25 21:26:43 -0500

    Styles for pokeTable page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`} 
+        // style={{ backgroundColor: "#DC0A2D" }}
       >
         <PokemonProvider>
           {children}
--- src/app/pokeTable/page.tsx
+++ src/app/pokeTable/page.tsx
@@ -8,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import Header from '../Header';
 
 interface PokemonTableProps {
   id: number;
@@ -50,28 +51,30 @@ export default async function PokemonTable({}: PokemonTableProps) {
   );
 
   return (
-    <main className='p-3 mx-auto' style={{ maxWidth: '29rem' }}>
-      <h1>Pokedex Card Grid view</h1>
-      <div className="grid grid-cols-3 gap-4 row-start-2 justify-center items-center w-full p-3 rounded-lg bg-slate-300" >
-        {detailedPokemonList.map((pokemon) => (
-          <Card key={pokemon.name} className="flex flex-col justify-center w-auto">
-            <CardHeader className='flex flex-row justify-end p-0 pt-1 pe-2'>
-              <CardTitle className='font-thin'>{pokemon.id}</CardTitle>
-            </CardHeader>
-            <CardContent className='p-0'>
-              <Image className='mx-auto pb-1' src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} width={70} height={70} />
-              {/* <CardDescription>
-                <p>HP: {pokemon.stats[0].base_stat}</p>
-                <p>Attack: {pokemon.stats[1].base_stat}</p>
-                <p>Defense: {pokemon.stats[2].base_stat}</p>
-              </CardDescription> */}
-            </CardContent>
-            <CardFooter className='p-0 pb-1'>
-              <CardTitle className='mx-auto font-normal'>{pokemon.name}</CardTitle>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    </main>
+    <>
+      <Header />
+      <main className='p-3 mx-auto' style={{ maxWidth: '29rem' }}>
+        <div className="grid grid-cols-3 gap-4 row-start-2 justify-center items-center w-full p-3 rounded-lg bg-white inset-shadow-sm" >
+          {detailedPokemonList.map((pokemon) => (
+            <Card key={pokemon.name} className="flex flex-col justify-center w-auto shadow-x">
+              <CardHeader className='flex flex-row justify-end p-0 pt-1 pe-2'>
+                <CardTitle className='font-thin'>{pokemon.id}</CardTitle>
+              </CardHeader>
+              <CardContent className='p-0'>
+                <Image className='mx-auto pb-1' src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} width={70} height={70} />
+                {/* <CardDescription>
+                  <p>HP: {pokemon.stats[0].base_stat}</p>
+                  <p>Attack: {pokemon.stats[1].base_stat}</p>
+                  <p>Defense: {pokemon.stats[2].base_stat}</p>
+                </CardDescription> */}
+              </CardContent>
+              <CardFooter className='p-0 pb-1 pt-1 rounded-lg bg-gray-100'>
+                <CardTitle className='mx-auto font-normal'>{pokemon.name}</CardTitle>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
