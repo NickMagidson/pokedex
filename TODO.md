8dfc7df323aed96f9e27be7119e5018e538384af
Nick <nickmagi24@gmail.com>
2025-02-25 22:26:52 -0500

    Add todo list

---
+++ TODO.md
@@ -0,0 +1,27 @@
+Pokedex
+
+Styling is still in experimentation
+  - Classic pokedex red
+  - Future glassmorphism?
+  - Background image
+
+
+
+1. PokeCard Page
+  - Body should have full height and no overflow
+  - Fixed height and over flow for grid. Z-indexs
+  - Add footer with enough room for searchbard
+  - Convert Cards into framer motion divs
+
+
+2. Autocomplete
+  - Install Mui Autocomplete
+  - Refactor SearchPokemon for that in lieu of Shad/Cn
+  - Combine the card render and search into one page. Console.log the click for testing
+
+
+3. Page per Pokemon
+  - On Card click, route to another page of that specific pokemon
+
+
+4. Filter cards by type
--- src/app/pokeTable/page.tsx
+++ src/app/pokeTable/page.tsx
@@ -74,6 +74,23 @@ export default async function PokemonTable({}: PokemonTableProps) {
             </Card>
           ))}
         </div>
+        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nickmagidson.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/moon.png"
+            alt="Moon logo"
+            width={20}
+            height={20}
+          />
+          Built by Nick Magidson →
+        </a>
+      </footer>
       </main>
     </>
   );
