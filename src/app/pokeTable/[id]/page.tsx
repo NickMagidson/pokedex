dc20727a0f4c02d63257b177227e43d0df1c417c
Nick <nickmagi24@gmail.com>
2025-03-05 23:48:41 -0500

    added next image

--- src/app/pokeTable/[id]/page.tsx
+++ src/app/pokeTable/[id]/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import Image from 'next/image';
 import Header from '../../Header'
 
 export default async function PokemonDetail(props: { params: Promise<{ id: string }> }) {
@@ -12,12 +14,12 @@ export default async function PokemonDetail(props: { params: Promise<{ id: strin
       <Header />
       <div className="p-6">
         <h1 className="text-3xl font-bold capitalize">{pokemon.name}</h1>
-        <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} className="w-48 h-48" />
+        <Image src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} width={200} height={200} />
         <p>Height: {pokemon.height}</p>
         <p>Weight: {pokemon.weight}</p>
         <p>Base experience: {pokemon.base_experience}</p>
       </div>
     </>
-
+  
   );
 }
