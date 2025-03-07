f75496d9e3755b9db50717ce1bb0433cb36a253f
Nick <nickmagi24@gmail.com>
2025-03-07 12:15:58 -0500

    Weight and height info

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -57,8 +57,8 @@ export default async function PokemonDetail(props: { params: Promise<{ id: strin
       <header className='flex flex-row justify-between items-center gap-2 p-3 w-full'>
         <Link href="/" className='flex flex-row items-center gap-3'>
           <Image src="/arrow_back.png" alt="Back Arrow" width={30} height={30} />
-          <h1 className='font-bold text-3xl capitalize'>{pokemon.name}</h1>
         </Link>
+        <h1 className='font-bold text-3xl capitalize'>{pokemon.name}</h1>
         <p className='font-bold mr-1 '># {pokemon.id}</p>
       </header>
 
@@ -73,7 +73,7 @@ export default async function PokemonDetail(props: { params: Promise<{ id: strin
           height={200} />
 
         <div 
-          className="gap-4 flex-grow row-start-2 justify-center items-center w-11/12 p-3 mx-auto
+          className="gap-5 flex-grow row-start-2 justify-center items-center w-11/12 p-3 mx-auto
           rounded-lg bg-white shadow-inner" 
           style={{ 
             boxShadow: 'rgba(0, 0, 0, 0.15) 0px 2px 4px inset, rgba(0, 0, 0, 0.65) 0px 2px 8px inset' 
@@ -92,7 +92,36 @@ export default async function PokemonDetail(props: { params: Promise<{ id: strin
             ))}
           </div>
 
+            <h3 className='font-bold text-center' style={{ color: getTypeColor(pokemon.types[0].type.name) }}>About</h3>
 
+            <div className="flex justify-between w-full py-3 border-t-2 border-b-2 mt-4">
+              <div className="flex flex-col items-center flex-1 h-full pt-2">
+
+                <div className='flex flex-row items-center gap-2'>
+                  <Image src="/weight.png" alt="Weight" width={18} height={18} />
+                  
+                  <p className='text-black mt-auto'>{pokemon.weight / 10} kg</p> {/* weight in kilograms */}
+                </div>
+                <div className='h-2'></div>
+                <p className="text-sm font-light text-gray-500">Weight</p>
+              </div>
+              <div className="border-x-2 px-4">
+
+                <div className="flex flex-col items-center pt-2">
+
+                  <div className='flex flex-row items-center gap-2'>
+                    <Image src="/height.png" alt="Height" width={18} height={18} />
+                    <p className='text-black mt-auto'>{pokemon.height / 10} m</p> {/* height in meters */}
+                  </div>
+                  <div className='h-2'></div>
+                  <p className="text-sm font-light text-gray-500">Height</p>
+                </div>
+              </div>
+              <div className="flex flex-col items-center flex-1">
+                <p>Choose a move</p> {/* Placeholder for dropdown */}
+                <p className="text-sm font-light text-gray-500">Moves</p>
+              </div>
+            </div>
 
 
           {/* <h1 className="text-3xl font-bold capitalize">{pokemon.name}</h1> */}
